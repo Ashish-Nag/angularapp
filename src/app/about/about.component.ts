38d8a46676a9ac639bdf286f5d93f180aec3df9c
Ashish Nag <ashish@nag.com>
2022-01-10 20:26:24 +0530

    learned directives, next> services

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  
+  // declaring a variable to use in the if else block written in HTML component.
+  public age:number = 20;
+  // declaring another variable for switch case statement.
+  public location: string = "Delhi";
+  // Creating an array to use the ngFor statement.
+  public myArr = ["Ashish", "Kumar", "Swati", "Rani", "Sahuji", "Muckhiya Ji"];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public xMas = "Click for Surprise!";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  clickMe():void{
+    console.log("the button got clicked bro!");
+    this.xMas = "Merry Christmas and a Happy New Year bro!!";
+    setTimeout(() => {
+      this.xMas= "Click for Surprise!";
+    }, 5000);
+  }
 }
