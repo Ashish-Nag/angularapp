847e428e8b55bcfe3ed65324d3f4d02224b851ab
Ashish Nag <ashish@nag.com>
2022-01-11 11:24:52 +0530

    added httpClient library as service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public xMas = "Click for Surprise!";
-  constructor() { }
+  posts$= {};
+  constructor(private api: ApiService) {
+    this.api.checkIfWorks();
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    return this.api.getData().subscribe(
+      api => this.posts$ = api
+
+      // TODO learn how to interpolate data into html using angular directives. ngFor isn't for objects.
+    );
   }
   clickMe():void{
     console.log("the button got clicked bro!");
